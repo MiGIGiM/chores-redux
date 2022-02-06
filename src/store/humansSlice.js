d6a5ef2968faf14a52689f9ec42a295d76124675
Miguel Rodriguez <fm.rodriguezg@hotmail.com>
2022-02-06 10:55:17 -0600

    ✨ Creating Redux app

---
+++ src/store/humansSlice.js
@@ -0,0 +1,38 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { tasksSlice } from "./tasksSlice";
+
+const createHuman = (name) => ({
+    id: nanoid(),
+    name,
+    taskIds: [],
+})
+
+const initialState = [
+  createHuman('Steve'),
+  createHuman('Bob'), 
+  createHuman('Alice'),
+  createHuman('John'),
+];
+
+export const humansSlice = createSlice({
+  name: 'humans',
+  initialState,
+  reducers: {
+    add: (state, action) => {
+      state.push(createHuman(action.payload));
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(tasksSlice.actions.assignToUser, (state, action) => {
+      for (const human of state) {
+        if (human.id === action.payload.humanId) {
+          human.taskIds.push(action.payload.taskId)
+        } else {
+          human.taskIds = human.taskIds.filter(
+            (id) => id !== action.payload.taskId
+          )
+        }
+      }
+    })
+  }
+});
---
+++ src/store/tasksSlice.js
@@ -0,0 +1,36 @@
+import { createAction, createSlice, nanoid } from "@reduxjs/toolkit";
+
+const createTask = (title) => ({
+  id: nanoid(),
+  title,
+  completed: false,
+  assignedTo: '',
+});
+
+const initialState = [
+  createTask('Order more energy drinks'),
+  createTask('Write a cool todo app'),
+]
+
+export const tasksSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    add: (state, action) => {
+      const task = createTask(action.payload);
+      state.push(task);
+    },
+    toggle: (state, action) => {
+      const task = state.find(task => task.id === action.payload.taskId);
+      task.completed = action.payload.completed;
+    },
+    assignToUser: (state, action) => {
+      const task = state.find(task => task.id === action.payload.taskId);
+      task.assignedTo = action.payload.humanId;
+    }
+  }
+})
+
+export const toggleTask = createAction('tasks/toggle', (taskId, completed) => ({
+  payload: { taskId, completed }
+}));
